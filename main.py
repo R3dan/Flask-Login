6697dc6bea683a71a8ebfcd5df688570600df382
R3dan <140912551+R3dan@users.noreply.github.com>
2024-03-18 22:11:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,128 +1,71 @@
 import flask
 from flask import session
+from flask_sqlalchemy import SQLAlchemy as SQL
 from cryptography.fernet import Fernet
-import random as r
-import pandas as pd
-from datetime import datetime as dt
+from flask_sqlalchemy import SQLAlchemy as SQL
 
-JSON = "JSON"
 
 class _ValueError(ValueError):
     pass
 
-class User:
-    def __init__(self, id, username, password, history, session, pandas, **values) -> None:
-        self.id = id
-        self.username = username
-        self.password = password
-        self.values = values
-        self.login_history = history
-        self.session_id = session
-        self.pandas = pandas
-        self.pd_df=pd.DataFrame(pandas)
+def get_database(db):  
+    class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.Text, nullable = False)
+        password = db.Column(db.Text, nullable=False)
+        login_history = db.Column(db.PickleType, nullable=False)
+
+    return User
 
 class Login:
 
     def __init__(
         self,
         app: flask.Flask,
-        db_ret: list,
-        database: str = None,
-        DBtype: str = None,
+        db:SQL = SQL(),
         KEY: bytes = None,
+        database = None,
         default_level=0,
-        login_function = None,
-        sign_up_function = None,
     ) -> None:
         self.app = app
-        self.dbFile = database
-        self.db_type = DBtype
-        self.db_ret = db_ret
         self.level = default_level
         self.key=1
+        app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.db'
+        self.db = db
+        if not database:
+            self.database=get_database(db)
 
-
-        if KEY == None:
-            self.KEY = Fernet.generate_key()
-        else:
-            self.KEY = KEY
-
-        self.fernet = Fernet(self.KEY)
-
-        try:
-            self.load()
-        except:
-            pass
-
-        self.db = pd.DataFrame()
-
-        if self.db_type == JSON:
-            import json
-            with open(database, "r") as f:
-                j=json.loads(f.read())
-                for i in j:
-                    self.sign_up(**i)
-
-        self.sessions = {}
 
     def login(self, **values):
-        x = self.db
-        for i in self.db_ret:
-            x = x.loc[x[i] == str(values[i])]
-        x = x.iloc[0]
-        
-        session_id = f"{dt.now().strftime("%d.%m.%y %H:%M:%S")}-{flask.request.remote_addr}-{r.randint(0, 10000000)}"
-        self.sessions[session_id] = x.iloc[2]
-        flask.session["session-key"] = str(session_id)
-        user = User(x.iloc[2], x.iloc[0], x.iloc[1], [], session_id, x)
-        self.user = user
+        x = self.database
+        user = x.query.filter(**values)[0]
+        #app=flask.current_app
+        flask.session["user"] = user
         return user
 
-    def save(self):
-        if self.db_type == JSON:
-            js = str(self.db.to_json()).encode()
-
-            d = self.fernet.encrypt(js)
-            with open("login.db", "wb") as f:
-                f.write(d)
-
-
-    def load(self, fernet, database):
-        
-        with open("login.db", "rb") as f:
-            print(self.fernet.decrypt(f.read()).decode())
-
     def sign_up(self, **values):
-        self.key += 1
-        values["uuid"]=self.key
-        values["level"]=values.get("level", self.level)
-        self.db=pd.concat([self.db, pd.DataFrame(values, index=[0])], ignore_index=True)
-        self.db.reset_index()
-        
+        user=self.database(**values)
+        self.db.session.add(user)
+        self.db.session.commit()
         return True
     
 
     def lock_page(self, index, value, refrence):
         try:
-            sess = flask.session.get("session-key")
-            uuid = self.sessions.get(sess)
-
-
-
-            user = self.db.loc[self.db["uuid"] == uuid].iloc[0]
+            user = flask.session.get("user")
             types = ["g", "l", "e", "ge", "le"]
             if refrence in types:
                 ref = refrence
                 if ref == "g":
-                    return user[index]>value
+                    return user.__dict__[index]>value
                 if ref == "l":
-                    return user[index]<value
+                    return user.__dict__[index]<value
                 if ref == "e":
-                    return user[index]==value
+                    return user.__dict__[index]==value
                 if ref == "ge":
-                    return user[index]>=value
+                    return user.__dict__[index]>=value
                 if ref == "le":
-                    return user[index]<=value
+                    return user.__dict__[index]<=value
                 return False
             else:
                 raise _ValueError(f"Refrerence {refrence} must be one of {types}")
@@ -132,5 +75,26 @@ class Login:
             return False
         
     def sign_out(self):
-        s=session.pop("session-key")
-        self.sessions.pop(s)
+        try:
+            u=session.pop("user")
+            return u, True
+        except:
+            return False
+
+    def delete_user(self) -> bool:
+        try:
+            user = flask.session.get("user")
+            if not user:
+                return False
+            self.db.session.delete(user)
+            self.db.session.commit()
+            return True
+        except:
+            return False
+        
+    def login_required(self):
+        user = flask.session.get("user")
+        if user:
+            return True
+        else:
+            return False
